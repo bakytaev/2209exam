1793479e510dffcb4aa5c001106de29ba83c1be4
bakytaev <bakytaev@gmail.com>
2022-09-22 23:39:12 +0600

    fourth

--- news/urls.py
+++ news/urls.py
@@ -10,7 +10,7 @@ router.register('statuses', views.StatusViewSet, basename='statuses')
 urlpatterns = [
     path('', include(router.urls)),
     path('news/<int:news_id>/comments/', views.CommentListCreateAPIView.as_view()),
-    path('news/<int:news_id>/comments/<int:pk>/', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
+    path('news/<int:news_id>/comments/<int:id>/', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
     path('news/<int:news_id>/<str:slug>/', views.PostNewsStatus.as_view()),
-    path('news/<int:news_id>/comment/<int:pk>/<str:slug>/', views.PostCommentStatus.as_view()),
+    path('news/<int:news_id>/comment/<int:comment_id>/<str:slug>/', views.PostCommentStatus.as_view()),
 ]
